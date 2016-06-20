35a90e343fdcfe4d77006d974c89922fba3e41a0
Tommy Parnell <tparnell8@gmail.com>
2016-06-20 19:43:05 -0400

    fix deps and what not

---
+++ src/CompressR.WebApiUnitTests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CompressR.WebApiUnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Passing()
+        {
+        }
+    }
+}
